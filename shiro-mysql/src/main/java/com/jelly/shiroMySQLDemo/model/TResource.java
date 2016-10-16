23459e09ebca36053609f8698e7b95b5c04277c1
jelly-liu <collonn@126.com>
2016-10-16 18:04:51 +0800

    add mybatis POJO and MAPPER

---
+++ shiro-mysql/src/main/java/com/jelly/shiroMySQLDemo/model/TResource.java
@@ -0,0 +1,43 @@
+
+/**
+* Author:Collonn, Email:collonn@126.com, QQ:195358385
+*/
+package com.jelly.shiroMySQLDemo.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class TResource {
+	public static final String Insert = "com.jelly.shiroMySQLDemo.model.TResource.Insert";
+	public static final String Update = "com.jelly.shiroMySQLDemo.model.TResource.Update";
+	public static final String UpdateWithNull = "com.jelly.shiroMySQLDemo.model.TResource.UpdateWithNull";
+	public static final String DeleteByPk = "com.jelly.shiroMySQLDemo.model.TResource.DeleteByPk";
+	public static final String DeleteByPojo = "com.jelly.shiroMySQLDemo.model.TResource.DeleteByPojo";
+	public static final String Select = "com.jelly.shiroMySQLDemo.model.TResource.Select";
+	public static final String SelectCount = "com.jelly.shiroMySQLDemo.model.TResource.SelectCount";
+	public static final String SelectByPk = "com.jelly.shiroMySQLDemo.model.TResource.SelectByPk";
+	
+	private String id;
+	
+	private String reName;
+
+
+	public TResource setId (String id) {
+		this.id = id;
+		return this;
+	}
+	
+	public String getId () {
+		return this.id;
+	}
+
+	public TResource setReName (String reName) {
+		this.reName = reName;
+		return this;
+	}
+	
+	public String getReName () {
+		return this.reName;
+	}
+
+}
---
+++ shiro-mysql/src/main/resources/shiro.sql
@@ -0,0 +1,110 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.16 : Database - shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shiro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `shiro`;
+
+/*Table structure for table `t_permission` */
+
+DROP TABLE IF EXISTS `t_permission`;
+
+CREATE TABLE `t_permission` (
+  `id` varchar(16) NOT NULL,
+  `pe_name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_permission` */
+
+/*Table structure for table `t_resource` */
+
+DROP TABLE IF EXISTS `t_resource`;
+
+CREATE TABLE `t_resource` (
+  `id` varchar(16) NOT NULL,
+  `re_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_resource` */
+
+/*Table structure for table `t_role` */
+
+DROP TABLE IF EXISTS `t_role`;
+
+CREATE TABLE `t_role` (
+  `id` varchar(16) NOT NULL,
+  `ro_name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role` */
+
+/*Table structure for table `t_role_permission` */
+
+DROP TABLE IF EXISTS `t_role_permission`;
+
+CREATE TABLE `t_role_permission` (
+  `id` varchar(16) NOT NULL,
+  `role_id` varchar(16) NOT NULL,
+  `permission_id` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_role_permission` */
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` varchar(16) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+/*Table structure for table `t_user_resource` */
+
+DROP TABLE IF EXISTS `t_user_resource`;
+
+CREATE TABLE `t_user_resource` (
+  `id` varchar(16) NOT NULL,
+  `user_id` varchar(16) NOT NULL,
+  `resource_id` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user_resource` */
+
+/*Table structure for table `t_user_role` */
+
+DROP TABLE IF EXISTS `t_user_role`;
+
+CREATE TABLE `t_user_role` (
+  `id` varchar(16) NOT NULL,
+  `user_id` varchar(16) NOT NULL,
+  `role_id` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
